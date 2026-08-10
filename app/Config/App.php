dfa39ee3030cf76adb60fa7ce426e9f3314fe0ac
Lance Miller <lanceverstappen30@gmail.com>
2026-08-10 16:29:38 +0800

    Hardcode production baseURL for Render deployment

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = '';
+    public string $baseURL = 'http://dappmc.onrender.com/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
